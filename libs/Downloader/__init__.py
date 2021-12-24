4b210e2358a6fca4c7f2d7f7acae8889e1f9a95c
Majroch <jakuboch4@gmail.com>
2021-12-24 13:29:59 +0100

    feat: master Update scripts and requirements

--- libs/Downloader/__init__.py
+++ libs/Downloader/__init__.py
@@ -3,35 +3,39 @@ from .url import identifyUrl
 import importlib
 import os
 
-class Browser: # pylint: disable=function-redefined
-    def __init__(self, links: list, destination: str="./video/"):
+
+class Browser:  # pylint: disable=function-redefined
+    def __init__(self, links: list, destination: str = "./video/"):
         self.links = links
         self.destination = destination
 
         mods = {}
         modules = {}
         for l in self.links:
-            module = identifyUrl(l) # pylint: disable=undefined-variable
-            if not module in mods:
+            module = identifyUrl(l)  # pylint: disable=undefined-variable
+            if module not in mods:
                 mods[module] = 1
-        
+
         current_pkg = os.path.dirname(os.path.abspath(__file__)).replace(os.getcwd(), "")[1:].replace("/", ".")
         for key in mods:
             try:
                 modules[key] = importlib.import_module("." + key, current_pkg)
-            except ImportError:
-                print("No Module named: " + key + ". Ommiting...")
+            except ImportError as error:
+                print("Cannot import module: " + key + ". Ommiting...")
+                print(f"Error message: {str(error)}")
 
         last_driver = None
+        downloader = None
         for l in links:
-            module = identifyUrl(l) # pylint: disable=undefined-variable
+            module = identifyUrl(l)  # pylint: disable=undefined-variable
             if module in modules.keys():
                 try:
                     if not last_driver == module:
                         try:
                             downloader.driver.close()
-                        except:
+                        except AttributeError:
                             pass
+                        finally:
                             downloader = eval(module + ".Downloader()")
 
                     downloader.download([l], destination)
